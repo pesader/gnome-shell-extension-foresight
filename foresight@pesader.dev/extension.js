56bae3a9e6a9f285e288948ca6f12740ecf3c504
Pedro Sader Azevedo <pedro.saderazevedo@lnbr.cnpem.br>
2025-02-25 10:35:29 -0300

    refactor(extension): improve code structure with new Foresight class
    Closes: https://github.com/pesader/gnome-shell-extension-foresight/issues/4

--- foresight@pesader.dev/extension.js
+++ foresight@pesader.dev/extension.js
@@ -8,111 +8,130 @@ import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 const DESTROY_WINDOW_ANIMATION_TIME = 150;
 const DIALOG_DESTROY_WINDOW_ANIMATION_TIME = 100;
 
-let _signal = [];
-let _manager, _workspace;
-let _activatedByExtension = false;
-
-const acceptedWindowTypes = [Meta.WindowType.NORMAL, Meta.WindowType.DIALOG, Meta.WindowType.MODAL_DIALOG];
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function windowAccepted(window) {
-    if (window.is_hidden() || acceptedWindowTypes.indexOf(window.get_window_type()) === -1)
-        return false;
-
-    return true;
-}
-
-function hideActivities() {
-    if (_activatedByExtension)
-        Main.overview.hide();
-}
-
-function showActivities() {
-    if (_workspace.list_windows().filter(window => windowAccepted(window)).length === 0) {
-        Main.overview.show();
-        _activatedByExtension = true;
+class Foresight {
+    constructor(workspaceManager) {
+        this._signal = [];
+        this._activatedByExtension = false;
+        this._workspaceManager = workspaceManager;
+        this._currentWorkspace = this._workspaceManager.get_active_workspace();
     }
-}
 
-function getWindowCloseAnimationTime(window) {
-    let animationTime;
+    _connectWorkspaceSignals() {
+        this._signal['window-removed'] = this._currentWorkspace.connect(
+            'window-removed',
+            (workspace, window) => this._windowRemoved(workspace, window)
+        );
+    }
 
-    // If animations are disabled, then the animation time is zero
-    if (!St.Settings.get().enable_animations)
-        animationTime = 0;
+    _disconnectWorkspaceSignals() {
+        if (this._signal['window-removed'])
+            this._currentWorkspace.disconnect(this._signal['window-removed']);
+    }
 
-    // Otherwise, the animation time depends on the type of window
-    else if (window.get_window_type() === Meta.WindowType.NORMAL)
-        animationTime = DESTROY_WINDOW_ANIMATION_TIME;
-    else
-        animationTime = DIALOG_DESTROY_WINDOW_ANIMATION_TIME;
+    connectSignals() {
+        this._connectWorkspaceSignals();
 
-    return animationTime;
-}
+        this._signal['workspace-switched'] = this._workspaceManager.connect('workspace-switched', () => this._workspaceSwitched());
+        this._signal['overview-hidden'] = Main.overview.connect('hidden', () => this._overviewHidden());
+    }
 
-async function windowRemoved(workspace, window) {
-    if (workspace !== _workspace)
-        return;
+    disconnectSignals() {
+        this._disconnectWorkspaceSignals();
 
-    if (!windowAccepted(window))
-        return;
+        Main.overview.disconnect(this._signal['overview-hidden']);
+        this._workspaceManager.disconnect(this._signal['workspace-switched']);
+    }
 
-    await sleep(getWindowCloseAnimationTime(window));
-    showActivities();
-}
 
-function disconnectWindowSignals() {
-    if (_signal['window-added'])
-        _workspace.disconnect(_signal['window-added']);
+    _sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-    if (_signal['window-removed'])
-        _workspace.disconnect(_signal['window-removed']);
-}
+    _windowAccepted(window) {
+        const acceptedWindowTypes = [Meta.WindowType.NORMAL, Meta.WindowType.DIALOG, Meta.WindowType.MODAL_DIALOG];
+        if (window.is_hidden() || acceptedWindowTypes.indexOf(window.get_window_type()) === -1)
+            return false;
 
-function getWorkspace() {
-    _workspace = _manager.get_active_workspace();
+        return true;
+    }
 
-    _signal['window-removed'] = _workspace.connect('window-removed', (workspace, window) => windowRemoved(workspace, window));
-}
+    _hideActivities() {
+        if (this._activatedByExtension)
+            Main.overview.hide();
+    }
 
-function checkWorkspace() {
-    disconnectWindowSignals();
+    _showActivities() {
+        if (this._currentWorkspace.list_windows().filter(window => this._windowAccepted(window)).length === 0) {
+            Main.overview.show();
+            this._activatedByExtension = true;
+        }
+    }
 
-    getWorkspace();
+    _getWindowCloseAnimationTime(window) {
+        let animationTime;
 
-    if ((_workspace.list_windows().filter(window => windowAccepted(window)).length > 0 || _workspace.index() === 0) && !Main.overview.dash.showAppsButton.checked)
-        hideActivities();
-    else if (!Main.overview.visible)
-        showActivities();
-}
+        // If animations are disabled, then the animation time is zero
+        if (!St.Settings.get().enable_animations)
+            animationTime = 0;
 
-function overviewHidden() {
-    _activatedByExtension = false;
+        // Otherwise, the animation time depends on the type of window
+        else if (window.get_window_type() === Meta.WindowType.NORMAL)
+            animationTime = DESTROY_WINDOW_ANIMATION_TIME;
+        else
+            animationTime = DIALOG_DESTROY_WINDOW_ANIMATION_TIME;
+
+        return animationTime;
+    }
+
+    async _windowRemoved(workspace, window) {
+        if (workspace !== this._currentWorkspace)
+            return;
+
+        if (!this._windowAccepted(window))
+            return;
+
+        await this._sleep(this._getWindowCloseAnimationTime(window));
+        this._showActivities();
+    }
+
+    _workspaceSwitched() {
+        this._disconnectWorkspaceSignals();
+
+        this._currentWorkspace = this._workspaceManager.get_active_workspace();
+        this._connectWorkspaceSignals();
+
+        if ((this._currentWorkspace.list_windows().filter(window => this._windowAccepted(window)).length > 0 || this._currentWorkspace.index() === 0) && !Main.overview.dash.showAppsButton.checked)
+            this._hideActivities();
+        else if (!Main.overview.visible)
+            this._showActivities();
+    }
+
+    _overviewHidden() {
+        this._activatedByExtension = false;
+    }
+
+    destroy() {
+        this._signal = null;
+        this._activatedByExtension = null;
+        this._workspaceManager = null;
+        this._currentWorkspace = null;
+    }
 }
 
 export default class ShowApplicationViewWhenWorkspaceEmptyExtension extends Extension {
     constructor(metadata) {
         super(metadata);
-        _manager = global.screen;
-        if (_manager === undefined)
-            _manager = global.workspace_manager;
+        const workspaceManager = global.workspace_manager;
+        this._foresight = new Foresight(workspaceManager);
     }
 
     enable() {
-        _activatedByExtension = false;
-        getWorkspace();
-
-        _signal['workspace-switched'] = _manager.connect('workspace-switched', checkWorkspace);
-        _signal['overview-hidden'] = Main.overview.connect('hidden', overviewHidden);
+        this._foresight.connectSignals();
     }
 
     disable() {
-        disconnectWindowSignals();
-
-        Main.overview.disconnect(_signal['overview-hidden']);
-        _manager.disconnect(_signal['workspace-switched']);
+        this._foresight.disconnectSignals();
+        this._foresight.destroy();
+        this._foresight = null;
     }
 }
